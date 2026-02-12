f1455562473bb89f08b972e1633753e05dd91890
Devang gupta <maybedevang29@gmail.com>
2026-02-12 12:50:43 +0530

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/Navbar';
 import Landing from './pages/Landing';
 import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </AuthProvider>
     </Router>
